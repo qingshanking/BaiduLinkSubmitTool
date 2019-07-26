d279b3bb9974cca61cc27e1faf2625244a82921b
QingShanKing <qingshanking@vip.qq.com>
2019-07-26 22:38:14 +0800

    修改基础提交成功，待完善。

--- Main/Main.Designer.cs
+++ Main/Main.Designer.cs
@@ -28,21 +28,164 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.skinLabel1 = new CCWin.SkinControl.SkinLabel();
+            this.token_label_text = new CCWin.SkinControl.SkinLabel();
+            this.Site_text = new CCWin.SkinControl.SkinWaterTextBox();
+            this.Token_text = new CCWin.SkinControl.SkinWaterTextBox();
+            this.rtfRichTextBox1 = new CCWin.SkinControl.RtfRichTextBox();
+            this.skinLabel3 = new CCWin.SkinControl.SkinLabel();
+            this.LinkSubmit = new CCWin.SkinControl.SkinButton();
+            this.skinLabel4 = new CCWin.SkinControl.SkinLabel();
+            this.Verification_Button = new CCWin.SkinControl.SkinButton();
             this.SuspendLayout();
+            // 
+            // skinLabel1
+            // 
+            this.skinLabel1.AutoSize = true;
+            this.skinLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.skinLabel1.BorderColor = System.Drawing.Color.White;
+            this.skinLabel1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.skinLabel1.Location = new System.Drawing.Point(32, 64);
+            this.skinLabel1.Name = "skinLabel1";
+            this.skinLabel1.Size = new System.Drawing.Size(44, 17);
+            this.skinLabel1.TabIndex = 0;
+            this.skinLabel1.Text = "域名：";
+            // 
+            // token_label_text
+            // 
+            this.token_label_text.AutoSize = true;
+            this.token_label_text.BackColor = System.Drawing.Color.Transparent;
+            this.token_label_text.BorderColor = System.Drawing.Color.White;
+            this.token_label_text.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.token_label_text.Location = new System.Drawing.Point(32, 100);
+            this.token_label_text.Name = "token_label_text";
+            this.token_label_text.Size = new System.Drawing.Size(44, 17);
+            this.token_label_text.TabIndex = 1;
+            this.token_label_text.Text = "密钥：";
+            // 
+            // Site_text
+            // 
+            this.Site_text.ForeColor = System.Drawing.SystemColors.ScrollBar;
+            this.Site_text.Location = new System.Drawing.Point(76, 62);
+            this.Site_text.Name = "Site_text";
+            this.Site_text.Size = new System.Drawing.Size(208, 21);
+            this.Site_text.TabIndex = 2;
+            this.Site_text.Text = "请输入百度搜索资源平台验证的站点";
+            this.Site_text.WaterColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(127)))), ((int)(((byte)(127)))));
+            this.Site_text.WaterText = "";
+            this.Site_text.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Site_text_MouseClick);
+            // 
+            // Token_text
+            // 
+            this.Token_text.ForeColor = System.Drawing.SystemColors.ScrollBar;
+            this.Token_text.Location = new System.Drawing.Point(76, 96);
+            this.Token_text.Name = "Token_text";
+            this.Token_text.Size = new System.Drawing.Size(208, 21);
+            this.Token_text.TabIndex = 3;
+            this.Token_text.Text = "请输入准入密钥";
+            this.Token_text.WaterColor = System.Drawing.Color.FromArgb(((int)(((byte)(127)))), ((int)(((byte)(127)))), ((int)(((byte)(127)))));
+            this.Token_text.WaterText = "";
+            this.Token_text.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Site_text_MouseClick);
+            // 
+            // rtfRichTextBox1
+            // 
+            this.rtfRichTextBox1.HiglightColor = CCWin.SkinControl.RtfRichTextBox.RtfColor.White;
+            this.rtfRichTextBox1.Location = new System.Drawing.Point(76, 138);
+            this.rtfRichTextBox1.Name = "rtfRichTextBox1";
+            this.rtfRichTextBox1.Size = new System.Drawing.Size(208, 244);
+            this.rtfRichTextBox1.TabIndex = 4;
+            this.rtfRichTextBox1.Text = "";
+            this.rtfRichTextBox1.TextColor = CCWin.SkinControl.RtfRichTextBox.RtfColor.Black;
+            // 
+            // skinLabel3
+            // 
+            this.skinLabel3.AutoSize = true;
+            this.skinLabel3.BackColor = System.Drawing.Color.Transparent;
+            this.skinLabel3.BorderColor = System.Drawing.Color.White;
+            this.skinLabel3.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.skinLabel3.Location = new System.Drawing.Point(32, 148);
+            this.skinLabel3.Name = "skinLabel3";
+            this.skinLabel3.Size = new System.Drawing.Size(44, 34);
+            this.skinLabel3.TabIndex = 5;
+            this.skinLabel3.Text = "链接\r\n地址：";
+            // 
+            // LinkSubmit
+            // 
+            this.LinkSubmit.BackColor = System.Drawing.Color.Transparent;
+            this.LinkSubmit.ControlState = CCWin.SkinClass.ControlState.Normal;
+            this.LinkSubmit.DownBack = null;
+            this.LinkSubmit.Location = new System.Drawing.Point(209, 405);
+            this.LinkSubmit.MouseBack = null;
+            this.LinkSubmit.Name = "LinkSubmit";
+            this.LinkSubmit.NormlBack = null;
+            this.LinkSubmit.Size = new System.Drawing.Size(75, 23);
+            this.LinkSubmit.TabIndex = 6;
+            this.LinkSubmit.Text = "提交";
+            this.LinkSubmit.UseVisualStyleBackColor = false;
+            this.LinkSubmit.Click += new System.EventHandler(this.LinkSubmit_Click);
+            // 
+            // skinLabel4
+            // 
+            this.skinLabel4.AutoSize = true;
+            this.skinLabel4.BackColor = System.Drawing.Color.Transparent;
+            this.skinLabel4.BorderColor = System.Drawing.Color.White;
+            this.skinLabel4.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.skinLabel4.Location = new System.Drawing.Point(7, 457);
+            this.skinLabel4.Name = "skinLabel4";
+            this.skinLabel4.Size = new System.Drawing.Size(44, 17);
+            this.skinLabel4.TabIndex = 7;
+            this.skinLabel4.Text = "状态：";
+            // 
+            // Verification_Button
+            // 
+            this.Verification_Button.BackColor = System.Drawing.Color.Transparent;
+            this.Verification_Button.ControlState = CCWin.SkinClass.ControlState.Normal;
+            this.Verification_Button.DownBack = null;
+            this.Verification_Button.Location = new System.Drawing.Point(76, 405);
+            this.Verification_Button.MouseBack = null;
+            this.Verification_Button.Name = "Verification_Button";
+            this.Verification_Button.NormlBack = null;
+            this.Verification_Button.Size = new System.Drawing.Size(75, 23);
+            this.Verification_Button.TabIndex = 8;
+            this.Verification_Button.Text = "验证";
+            this.Verification_Button.UseVisualStyleBackColor = false;
+            this.Verification_Button.Click += new System.EventHandler(this.Verification_Button_Click);
             // 
             // Main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(557, 450);
+            this.ClientSize = new System.Drawing.Size(344, 478);
+            this.Controls.Add(this.Verification_Button);
+            this.Controls.Add(this.skinLabel4);
+            this.Controls.Add(this.LinkSubmit);
+            this.Controls.Add(this.skinLabel3);
+            this.Controls.Add(this.rtfRichTextBox1);
+            this.Controls.Add(this.Token_text);
+            this.Controls.Add(this.Site_text);
+            this.Controls.Add(this.token_label_text);
+            this.Controls.Add(this.skinLabel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.Name = "Main";
             this.Text = "百度主动推送链接工具";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private CCWin.SkinControl.SkinLabel skinLabel1;
+        private CCWin.SkinControl.SkinLabel token_label_text;
+        private CCWin.SkinControl.SkinWaterTextBox Site_text;
+        private CCWin.SkinControl.SkinWaterTextBox Token_text;
+        private CCWin.SkinControl.RtfRichTextBox rtfRichTextBox1;
+        private CCWin.SkinControl.SkinLabel skinLabel3;
+        private CCWin.SkinControl.SkinButton LinkSubmit;
+        private CCWin.SkinControl.SkinLabel skinLabel4;
+        private CCWin.SkinControl.SkinButton Verification_Button;
     }
 }
 
--- Main/Main.cs
+++ Main/Main.cs
@@ -5,16 +5,149 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Windows.Forms;
 using CCWin;
+using System.Web;
 
 namespace Main
 {
+    
     public partial class Main : Skin_Mac
     {
         public Main()
         {
             InitializeComponent();
         }
+
+        private static List<string> ok_url = new List<string>();
+        /// <summary>
+        /// 鼠标点击隐藏默认数据
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Token_text_TextChanged(object sender, EventArgs e)
+        {
+            Token_text.Text = "";
+        }
+        private void Token_text_MouseClick(object sender, EventArgs e)
+        {
+            Token_text.Text = "";
+        }
+        /// <summary>
+        /// 鼠标点击隐藏默认数据
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Site_text_MouseClick(object sender, MouseEventArgs e)
+        {
+            Site_text.Text = "";
+        }
+
+        private void LinkSubmit_Click(object sender, EventArgs e)
+        {
+            if (ok_url.Count == 0)
+            {
+                MessageBox.Show("请先验证数据！"); return;
+            }
+            if (string.IsNullOrWhiteSpace(Site_text.Text))
+            {
+                MessageBox.Show("请输入搜索资源平台验证的站点(必须带http或者https)！"); return;
+            }
+            if (string.IsNullOrWhiteSpace(Token_text.Text))
+            {
+                MessageBox.Show("请输入在搜索资源平台申请的推送用的准入密钥！"); return;
+            }
+            var str = Linksubmit(Site_text.Text, Token_text.Text, ok_url);
+            MessageBox.Show(str);
+
+        }
+        private string Linksubmit(string site, string token, List<string> url)
+        {
+            HttpHelper http = new HttpHelper();
+            HttpItem item = new HttpItem()
+            {
+                URL = "http://data.zz.baidu.com/urls?site=" + site + "&token=" + token,//URL     必需项  
+                Method = "Post",//URL     可选项 默认为Get  
+                Timeout = 100000,//连接超时时间     可选项默认为100000  
+                ReadWriteTimeout = 30000,//写入Post数据超时时间     可选项默认为30000  
+                //IsToLower = false,//得到的HTML代码是否转成小写     可选项默认转小写  
+                Cookie = "",//字符串Cookie     可选项  
+                UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:18.0) Gecko/20100101 Firefox/18.0",//用户的浏览器类型，版本，操作系统     可选项有默认值  
+                Accept = "text/html, application/xhtml+xml, */*",//    可选项有默认值  
+                ContentType = "text/plain",//返回类型    可选项有默认值  
+                Referer = site,//来源URL     可选项 
+                Postdata = rtfRichTextBox1.Text,//Post数据     可选项GET时不需要写
+                ResultType = ResultType.String,//返回数据类型，是Byte还是String  
+                //CURLOPT_HTTPHEADER => array('Content-Type: text/plain'),
+            };
+            HttpResult result = http.GetHtml(item);
+            string html = result.Html;
+            string cookie = result.Cookie;
+            return html;
+        }
+        //封装的方法
+        static bool ExactMatch(string input, string match)
+        {
+            //bool contains = Regex.IsMatch("Hello1 Hello2", "Hello"); // 输出 false
+            return Regex.IsMatch(input, match);
+            //return Regex.IsMatch(input, string.Format(@"b{0}b", Regex.Escape(match)));
+        }
+        /// <summary>
+        /// 验证数据
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Verification_Button_Click(object sender, EventArgs e)
+        {
+            string _site_text = Site_text.Text;
+            if (string.IsNullOrWhiteSpace(_site_text))
+                return;
+            string url_txt = rtfRichTextBox1.Text;
+            string[] url_list_string = url_txt.Split('\n');//截取每行数据
+            int num = url_list_string.Count();
+            if (num == 0)
+                return;
+            int z = 0, y = 0;
+            for (int i = 0; i < num; i++)
+            {
+                if (ExactMatch(url_list_string[i], _site_text))
+                {
+                    ok_url.Add(url_list_string[i]);
+                    z++;
+                }
+                else
+                    y++;
+            }
+            skinLabel4.Text = "验证数据：" + z + "条正常，" + y + "条失败！";
+        }
+    }
+    /// <summary>
+    /// 返回成功时候
+    /// </summary>
+    public class Result_Sucess
+    {
+        /// <summary>
+        /// 成功推送的url条数
+        /// </summary>
+        public int success { get; set; }
+        /// <summary>
+        /// 当天剩余的可推送url条数
+        /// </summary>
+        public int remain { get; set; }
+    }
+    /// <summary>
+    /// 返回错误时候
+    /// </summary>
+    public class Result_Error
+    {
+        /// <summary>
+        /// 错误码，与状态码相同
+        /// </summary>
+        public int error { get; set; }
+        /// <summary>
+        /// 错误描述
+        /// </summary>
+        public string message { get; set; }
     }
 }
